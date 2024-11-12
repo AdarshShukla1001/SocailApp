c22dbeebb063a103c67c34f3dc63990c2f325fde
Adarsh Shukla <adarshshukla10012002@gmail.com>
2024-11-12 21:55:51 +0530

    Initial Commit

---
+++ SocailApp/SocailAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocailAppApp.swift
+//  SocailApp
+//
+//  Created by adarsh shukla on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SocailAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SocailAppTests/SocailAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  SocailAppTests.swift
+//  SocailAppTests
+//
+//  Created by adarsh shukla on 12/11/24.
+//
+
+import Testing
+
+struct SocailAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
